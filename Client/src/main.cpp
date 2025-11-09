8213eec8842ebae23b9faffb991fd846d360ebb7
Jack Newcomb <jackrnewcomb@gmail.com>
2025-11-09 08:35:12 -0500

    some init sfml stuff, structure for a client class and main

---
+++ Client/src/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+bool sanitizeInput(const std::vector<std::string> &userInput)
+{
+    std::string invalidMsg = "Invalid command line argument detected : " + userInput[1] +
+                             "\nPlease check your values and press any key to end the program!";
+    int port;
+    try
+    {
+        port = std::stoi(userInput[1]);
+    }
+    catch (...)
+    {
+        std::cout << invalidMsg;
+        return false;
+    }
+
+    if (port < 61000 || port > 65535)
+    {
+        std::cout << invalidMsg;
+        return false;
+    }
+    return true;
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 3)
+    {
+        std::cout << "Invalid number of args passed: Expected 2. Aborting\n";
+        return -1;
+    }
+
+    std::string ipAddress = argv[1];
+    std::string port = argv[2];
+
+    bool clean = sanitizeInput({ipAddress, port});
+    if (!clean)
+    {
+        return -1;
+    }
+
+    return 0;
+}
